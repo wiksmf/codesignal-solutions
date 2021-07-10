ee3b09ce1d66d9d64033013f691a55f90c169eec
wiksmf <wiksmf@gmail.com>
2021-07-10 21:31:04 +0200

    initial commit

--- Interview Practice/Arrays/sudoku2.js
+++ Interview Practice/Arrays/sudoku2.js
@@ -5,14 +5,14 @@ function sudoku2(grid) {
 function checkRow(grid) {
   return grid
     .map(el => el.filter(subEl => !isNaN(subEl) === true))
-    .every(el => el.length === new Set(el).size);
+    .map(el => el.length === new Set(el).size);
 }
 
 function checkColumn(grid) {
   let reverseGrid = [];
 
   for (let i = 0; i < grid.length; i++) {
-    reverseGrid.push([]);
+    reverseGrid.push([])
     for (let j = 0; j < grid[i].length; j++) {
       reverseGrid[i].push(grid[j][i]);
     }
@@ -22,19 +22,18 @@ function checkColumn(grid) {
 }
 
 function checkSubgrid(grid) {
-  let newGrid = [];
-
   for (let i = 0; i < grid.length; i += 3) {
     for (let j = 0; j < grid.length; j += 3) {
       let subgrid = [];
 
-      for (let k = i; k < i + 3; k++)
-        for (let l = j; l < j + 3; l++)
-          subgrid.push(grid[k][l]);
-
-      newGrid.push(subgrid);
+      for (let k = i; k < i + 3; k++) {
+        for (let l = j; l < j + 3; j++) {
+          subgrid.push(grid[k][l])
+        }
+      }
+      if (subgrid.length !== new Set(subgrid).size) return false;
     }
   }
 
-  return checkRow(newGrid);
+  return false;
 }
