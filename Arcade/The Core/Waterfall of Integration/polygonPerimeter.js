7f76f8a88145e6047cee95bf370cbaedc67a6dbe
wiksmf <wiksmf@gmail.com>
2021-09-09 20:47:04 +0200

    arcade: waterfall of integration - initial commit

---
+++ Arcade/The Core/Waterfall of Integration/polygonPerimeter.js
@@ -0,0 +1,14 @@
+function polygonPerimeter(matrix) {
+  let perimeter = 0;
+
+  for (let i = 0; i < matrix.length; i++)
+    for (let j = 0; j < matrix[0].length; j++)
+      if (matrix[i][j]) {
+        if (!matrix[i][j - 1]) perimeter++;
+        if (!matrix[i][j - 1]) perimeter++;
+        if (!matrix[i - 1] || !matrix[i - 1][j]) perimeter++;
+        if (!matrix[i + 1] || !matrix[i + 1][j]) perimeter++;
+      }
+
+  return perimeter;
+}
